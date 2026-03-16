661d2905f655c61201e5315f3ba4034634d609df
Myu007 <95171353+Myu007@users.noreply.github.com>
2026-03-16 17:26:51 +0600

    Update auth-check.js

--- api/auth-check.js
+++ api/auth-check.js
@@ -1,1 +1,21 @@
+const fs = require('fs');
+const path = require('path');
 
+module.exports = function handler(req, res) {
+  const cookies = req.headers.cookie || '';
+  const isAuthed = cookies.includes('auth=true');
+
+  if (isAuthed) {
+    try {
+      const filePath = path.join(process.cwd(), 'public', 'home.html');
+      const html = fs.readFileSync(filePath, 'utf8');
+      res.setHeader('Content-Type', 'text/html');
+      return res.status(200).send(html);
+    } catch (e) {
+      return res.status(500).send('Could not load page.');
+    }
+  }
+
+  res.setHeader('Location', '/');
+  return res.status(302).end();
+};
